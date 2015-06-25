e190e4cdcaa48537387c301ed53d0c1c5895cae3
Matthew Giardina <mlgiardina@me.com>
2015-06-25 17:22:58 -0500

    ran rake db:migrate

--- db/migrate/20150625215903_create_orders.rb
+++ db/migrate/20150625215903_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.belongs_to :user, index: true, foreign_key: true
+      t.boolean :completed, null: false, default: false
 
       t.timestamps null: false
     end
--- db/migrate/20150625215913_create_products.rb
+++ db/migrate/20150625215913_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :name, null: false
+      t.string :description, null: false, default: "product"
+      t.integer :inventory, null: false, default: 0
+      t.integer :price, null: false, default: 1
 
       t.timestamps null: false
     end
