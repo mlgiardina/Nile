fef4202e5cce6c3925c6c94cf6255d271cdda49c
Matthew Giardina <mlgiardina@me.com>
2015-06-28 21:12:00 -0500

    commented out signup page

--- app/assets/javascripts/components/signup.js.jsx
+++ app/assets/javascripts/components/signup.js.jsx
@@ -1,17 +1,17 @@
-var Signup = React.createClass({
-    render: function () {
-        return (
-            <section className='signup-form'>
-                <form name='signup' action='/users' method='post'>
-                    <ul>
-                        <li> <label>Name</label> <input type='name' placeholder='Steve Jobs' required name="name"/> </li>
-                        <li> <label>Email</label> <input type='email' placeholder='example@email.com' required name="email"/> </li>
-                        <li> <label>Password</label> <input type='password' required name="password"/> </li>
-                        <li> <label>Confirm Password</label> <input type='password' required name="password_confirmation"/> </li>
-                        <button type='submit' className='btn btn-default'>Sign Up</button>
-                    </ul>
-                </form>
-            </section>
-        );
-    }
-});
+//var Signup = React.createClass({
+//    render: function () {
+//        return (
+//            <section className='signup-form'>
+//                <form name='signup' action='/users' method='post'>
+//                    <ul>
+//                        <li> <label>Name</label> <input type='name' placeholder='Steve Jobs' required name="name"/> </li>
+//                        <li> <label>Email</label> <input type='email' placeholder='example@email.com' required name="email"/> </li>
+//                        <li> <label>Password</label> <input type='password' required name="password"/> </li>
+//                        <li> <label>Confirm Password</label> <input type='password' required name="password_confirmation"/> </li>
+//                        <button type='submit' className='btn btn-default'>Sign Up</button>
+//                    </ul>
+//                </form>
+//            </section>
+//        );
+//    }
+//});
